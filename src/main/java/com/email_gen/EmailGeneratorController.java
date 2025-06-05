95a7dc33aab2b146843e4776cc9a15bf4585d4ab
Young <kimyoh123@gmail.com>
2025-06-04 23:15:43 -0700

    cross origin for cors

--- src/main/java/com/email_gen/EmailGeneratorController.java
+++ src/main/java/com/email_gen/EmailGeneratorController.java
@@ -2,16 +2,14 @@ package com.email_gen;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 // /api/email/generate
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private EmailGeneratorService emailGeneratorService;
