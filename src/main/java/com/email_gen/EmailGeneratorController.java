c697499417cdb444a2783c42acc41adcfe16f5e9
Young <kimyoh123@gmail.com>
2025-06-04 23:05:55 -0700

    extracting gemini output

--- src/main/java/com/email_gen/EmailGeneratorController.java
+++ src/main/java/com/email_gen/EmailGeneratorController.java
@@ -1,16 +1,24 @@
 package com.email_gen;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+// /api/email/generate
 @RestController
 @RequestMapping("/api/email")
+@AllArgsConstructor
 public class EmailGeneratorController {
 
+    private EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
-        return ResponseEntity.ok("");
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
     }
 }
