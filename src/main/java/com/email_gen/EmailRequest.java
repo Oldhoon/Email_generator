1fe14b7ffed02846e27ce9576969707f97a5d140
Young <kimyoh123@gmail.com>
2025-06-07 21:56:30 -0700

    parent folder repo

--- src/main/java/com/email_gen/EmailRequest.java
+++
@@ -1,9 +0,0 @@
-package com.email_gen;
-
-import lombok.Data;
-
-@Data // generate getter setter constructor
-public class EmailRequest {
-    private String emailContent;
-    private String tone;
-}
